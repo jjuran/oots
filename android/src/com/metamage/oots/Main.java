965e16067bcf0fa8adf8b8b14eb6b6ed7f5a1754
Joshua Juran <jjuran@metamage.com>
2011-06-28 17:58:57 -0700

    Add minimal Android application
    The icon is the square link banner from here:
    
    http://www.giantitp.com/comics/LinkBanners.html
    
    http://www.giantitp.com/Images/ootssquare.gif

---
+++ android/src/com/metamage/oots/Main.java
@@ -0,0 +1,19 @@
+package com.metamage.oots;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public final class Main extends Activity
+{
+	
+	@Override
+	public void onCreate( Bundle savedInstanceState )
+	{
+		super.onCreate( savedInstanceState );
+		
+		setContentView( R.layout.main );
+	}
+	
+}
+
