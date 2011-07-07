b8aa53f38a0d0d58edf9e0c9343a71791e936b40
Joshua Juran <jjuran@metamage.com>
2011-07-07 05:19:50 -0700

    Link icon to the GitP Web site
    The icon is offered as a link banner, so make it a link.
    
    I would have linked to the page for the Order of the Stick if there were
    one.  The table of contents page listing the newest comic titles doesn't
    seem like the right target, so I simply used <http://www.giantitp.com/>.

--- android/src/com/metamage/oots/Main.java
+++ android/src/com/metamage/oots/Main.java
@@ -1,11 +1,19 @@
 package com.metamage.oots;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 
 public final class Main extends Activity
 {
+	
+	public void onClickWebSite( View v )
+	{
+		startActivity( new Intent( Intent.ACTION_VIEW, Uri.parse( Web.GITP ) ) );
+	}
 	
 	@Override
 	public void onCreate( Bundle savedInstanceState )
---
+++ android/src/com/metamage/oots/Web.java
@@ -0,0 +1,10 @@
+package com.metamage.oots;
+
+
+public final class Web
+{
+	
+	static final String GITP = "http://www.giantitp.com/";
+	
+}
+
